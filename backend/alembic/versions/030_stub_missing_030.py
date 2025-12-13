8d03d0f0db546253427b3f79ab1f3829014088ac
zloiws <zloiws@gmail.com>
2025-12-13 18:24:51 +0200

    chore(migrations): fix stub 030 down_revision to match 028_add_agent_conversations

--- backend/alembic/versions/030_stub_missing_030.py
+++ backend/alembic/versions/030_stub_missing_030.py
@@ -11,7 +11,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision: str = '030'
-down_revision: Union[str, None] = '028'
+down_revision: Union[str, None] = '028_add_agent_conversations'
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
